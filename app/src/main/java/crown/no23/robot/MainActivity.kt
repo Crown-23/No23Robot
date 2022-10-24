1f98cab1c503a5be3cae873ceca5745714b88e32
Crown-23 <2014385565@qq.com>
2022-10-24 15:18:43 +0800

    create project

---
+++ app/src/main/java/crown/no23/robot/MainActivity.kt
@@ -0,0 +1,11 @@
+package crown.no23.robot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
